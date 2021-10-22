685fac170f5a32467ee4fb120365363987ecaaca
MOH1709 <ahirwalmohit3216@gmail.com>
2021-10-22 11:33:38 +0530

    finalize project

---
+++ js/game.js
@@ -0,0 +1,72 @@
+const $ = (id) => document.getElementById(id);
+let jump = 0;
+let des = 0;
+let turn = 0;
+
+//--------------------------------------------------------------------
+function winPos() {
+  return ((des - 1) % (jump + 1)) + (jump + 1) * turn;
+}
+
+function AIMove(curNum) {
+  return Math.abs(winPos() - curNum);
+}
+
+function reset() {
+  window.history.go(-1);
+}
+
+//--------------------------------------------------------------------
+window.onload = () => {
+  jump = +localStorage.getItem("jump");
+  des = +localStorage.getItem("des");
+
+  $("desNum").innerHTML = des;
+  $("input").max = localStorage.getItem("jump");
+
+  $("choice").innerHTML = $("input").value;
+
+  if (winPos() < jump) {
+    $("aiChoice").innerHTML = "+" + AIMove(-1);
+    $("curNum").innerHTML = AIMove(-1);
+    turn = 1;
+  }
+};
+
+//--------------------------------------------------------------------
+$("input").onchange = (e) => {
+  $("choice").innerHTML = e.target.value;
+};
+
+//---------------------------------- next ----------------------------------
+$("next").onclick = () => {
+  let curVal = $("curNum").innerText;
+  let pos = +curVal;
+
+  pos += parseInt($("input").value);
+  $("curNum").value = pos;
+  $("curNum").innerHTML = pos;
+  if (pos >= des) {
+    alert("You Won🎉🤝");
+    reset();
+  }
+
+  //---------------------------------- AI Turn ----------------------------------
+
+  setTimeout(() => {
+    curVal = AIMove(pos - 1);
+    $("aiChoice").innerHTML = "+" + curVal;
+    $("curNum").innerHTML = pos + curVal;
+    if (pos + curVal == des) {
+      alert("AI choose : " + curVal + "\nAI Wins🎉🥳");
+      reset();
+    } else {
+      alert("AI's Choose : " + "+" + curVal);
+      turn++;
+    }
+  }, 50);
+};
+//---------------------------------- Reset ----------------------------------
+$("reset").onclick = () => {
+  reset();
+};
---
+++ js/main.js
@@ -0,0 +1,27 @@
+const $ = id => document.getElementById(id);
+
+window.onload = () => {
+  $("jumpVal").innerHTML = $("jump").value;
+  $("jump").max = Math.floor($("des").value / 4);
+}
+
+//---------------------------------- start ----------------------------------
+$("start").onclick = () => {
+  let destination = $("des").value;
+  let jump = $("jump").value;
+  
+  localStorage.setItem("des", destination);
+  localStorage.setItem("jump", jump);
+  window.open("../pages/gameScreen.html", "_self");
+};
+
+//---------------------------------- jump ----------------------------------
+$("jump").onchange = (e) => {
+  $("jumpVal").innerHTML = e.target.value;
+};
+
+//---------------------------------- destination ----------------------------------
+$("des").onchange = (e) => {
+  $("jump").max = Math.floor(e.target.value / 4);
+  $("jumpVal").innerHTML = $("jump").value;
+};
